400c3a0e604280e76270fdce64e4ccf06dd01a82
filagnes <48855556+filagnes@users.noreply.github.com>
2020-10-27 13:02:59 +0100

    additional commit

---
+++ form.php
@@ -0,0 +1,32 @@
+<?php
+//jeżeli zmienna "email" została wypełniona, wysyłamy wiadomość
+  if (isset($_REQUEST['email']))  {
+  
+  //Informację o emailu, na który będzie wysyłana wiadomość
+  $admin_email = "agafilam@gmail.com";
+  $email = $_REQUEST['email'];
+  $subject = $_REQUEST['subject'];
+  $comment = $_REQUEST['comment'];
+  
+  //wysyłamy email
+  mail($admin_email, "$subject", $comment, "From:" . $email);
+  
+  //komunikat potwierdzający
+  echo "Dziękujemy za kontakt z nami!";
+  }
+  
+  //jeżeli zmienna z wartością "email" nie została wypełniona pokazujemy ponownie formularz
+  else  {
+?>
+
+ <form method="post">
+  Email: <input name="email" type="text" /><br />
+  Temat: <input name="subject" type="text" /><br />
+  Wiadomość:<br />
+  <textarea name="comment" rows="15" cols="40"></textarea><br />
+  <input type="submit" value="Wyślij" />
+  </form>
+  
+<?php
+  }
+?>
