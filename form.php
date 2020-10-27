eebd06843642d06637ef0a603368bf0a728bc748
filagnes <48855556+filagnes@users.noreply.github.com>
2020-10-27 14:46:38 +0100

    commit

--- form.php
+++ form.php
@@ -19,13 +19,13 @@
   else  {
 ?>
 
- <form method="post">
+ <!-- <form method="post">
   Email: <input name="email" type="text" /><br />
   Temat: <input name="subject" type="text" /><br />
   Wiadomość:<br />
-  <textarea name="comment" rows="15" cols="40"></textarea><br />
+  <textarea name="comment" rows="5" cols="40"></textarea><br />
   <input type="submit" value="Wyślij" />
-  </form>
+  </form> -->
   
 <?php
   }
